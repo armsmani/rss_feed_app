3bd4459e80352ed1ed58b26552b35bd244d23ad5
Venkataramanan N <venkatarun.n@gmail.com>
2016-03-09 00:52:06 +0530

    * implemented the RSSFeed Paser and Display in HTML

--- rss_view/views.py
+++ rss_view/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rss_view.models import *
+
+import feedparser
+import json
 
 # Create your views here.
+def feedreader(request):
+    rssDetails = RssDetails.objects.all()
+    for rssDetail in rssDetails:
+        rssFeed = feedparser.parse(rssDetail.link)
+
+        for feed in rssFeed.entries:
+            news = News()
+            news.title = feed.title
+            news.content = feed.summary
+            news.link = feed.link
+            news.published_date = feed.published
+            news.save()
+    return render(request, 'home.html', {'result': "None"})
+
+
+def feed_render(request):
+    newsDetails = News.objects.all()
+    data = [{'title': news.title, 'summary': news.content, 'link': news.link}
+                for news in newsDetails]
+    result = json.dumps(data)
+    return HttpResponse(result, content_type="application/json")
--- veooz/settings.py
+++ veooz/settings.py
@@ -91,3 +91,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
